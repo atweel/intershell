45bd1fcf02c7b229cf78ba92358f0f76cd603687
Eduard Malakhov <eduard@atweel.com>
2020-04-14 13:39:01 +0700

    Fix primer to not fail if shortcut command does not exist.

--- packages/primer/sources/examples/shortcuts.ts
+++ packages/primer/sources/examples/shortcuts.ts
@@ -1,17 +1,39 @@
+import commandExists from 'command-exists';
 import { sh, bash, dash, zsh } from '@atweel/intershell';
 
-sh`echo "Hello from $0"`((error, stdout) => {
-    console.log(`${ stdout } (sh)`);
-});
+commandExists('sh')
+    .then(() => {
+        sh`echo "Hello from $0"`((error, stdout) => {
+            console.log(`${ stdout } (sh)`);
+        });
+    }).catch(() => {
+        console.log(`Command 'sh' is not available on this system.`);
+    });
 
-bash`echo "Hello from $0"`((error, stdout) => {
-    console.log(`${ stdout } (bash)`);
-});
+commandExists('bash')
+    .then(() => {
+        bash`echo "Hello from $0"`((error, stdout) => {
+            console.log(`${ stdout } (bash)`);
+        });
+    }).catch(() => {
+        console.log(`Command 'bash' is not available on this system.`);
+    });
 
-dash`echo "Hello from $0"`((error, stdout) => {
-    console.log(`${ stdout } (dash)`);
-});
+commandExists('dash')
+    .then(() => {
+        dash`echo "Hello from $0"`((error, stdout) => {
+            console.log(`${ stdout } (dash)`);
+        });
+    }).catch(() => {
+        console.log(`Command 'dash' is not available on this system.`);
+    });
 
-zsh`echo "Hello from $0"`((error, stdout) => {
-    console.log(`${ stdout } (zsh)`);
-});
+commandExists('zsh')
+    .then(() => {
+        zsh`echo "Hello from $0"`((error, stdout) => {
+            console.log(`${ stdout } (zsh)`);
+        });
+    }).catch(() => {
+        console.log(`Command 'zsh' is not available on this system.`);
+    });
+
